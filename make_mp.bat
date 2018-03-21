da1deaab5fec3bb582ab23e38c0110a332ffce03
Victor <pvvx@mail.ru>
2018-03-21 06:26:09 +0300

    update

--- make_mp.bat
+++
@@ -1,7 +0,0 @@
-set PATH=c:\MinGW\mingw64\bin;C:\MinGW\msys\1.0\bin;%PATH%
-rem mingw32-make.exe -j 20 -s clean
-set OTA_IDX=1
-mingw32-make.exe -s -j 20 mp
-set OTA_IDX=2
-mingw32-make.exe -s -j 20 mp
-pause
